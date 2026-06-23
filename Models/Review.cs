5d4800e2121dfe6ef5fc357be2db7e8e65586748
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-06-23 20:02:29 +0200

    create Review model class

--- Models/Review.cs
+++ Models/Review.cs
@@ -2,5 +2,8 @@ namespace MovieApi.Models;
 
 public class Review
 {
-    
+    int ReviewId {get;set;}
+    string ReviewerName {get;set;}
+    string Comment {get;set;}
+    int rating {get; set;}
 }
