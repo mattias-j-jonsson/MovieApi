415d8cc0e424167f263e4fda37f4a6a414da8cc7
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-06-23 20:00:07 +0200

    create the MovieDetails model class

--- Models/MovieDetails.cs
+++ Models/MovieDetails.cs
@@ -2,5 +2,8 @@ namespace MovieApi.Models;
 
 public class MovieDetails
 {
-    
+    int MovieDetailsId {get; set;}
+    string Synopsis {get; set;}
+    string Language {get;set;}
+    ulong Budget {get;set;}
 }
