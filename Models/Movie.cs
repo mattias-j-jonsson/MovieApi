49b935851d21fb0a8c947d65a629078865cdf858
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-06-23 20:04:21 +0200

    fix properties in Movie model class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,7 +7,7 @@ public class Movie
     DateOnly Year {get;set;}
     string Genre  {get;set;}
     TimeSpan Duration {get;set;}
-    MovieDetails movieDetails;
-    IEnumerable<Review> reviews;
-    IEnumerable<Actor> actors;
+    MovieDetails movieDetails {get;set;}
+    IEnumerable<Review> reviews {get; set;} = new List<Review>();
+    IEnumerable<Actor> actors {get; set;} = new List<Actor>();
 }
