a65fb3c0a1c77c0158e18556a434bd725ad99111
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-06-25 17:22:20 +0200

    add some annotation validation of model data

---
+++ Migrations/20260625151851_AnnotationValidation.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AnnotationValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewerName",
+                table: "Reviews",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Reviews",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewerName",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
--- Models/Review.cs
+++ Models/Review.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieApi.Models;
@@ -5,8 +6,15 @@ namespace MovieApi.Models;
 public class Review
 {
     public int ReviewId {get;set;}
+    
+    [Required]
+    [MaxLength(50)]
     public string ReviewerName {get;set;}
+
+    [MaxLength(200)]
     public string Comment {get;set;}
+
+    [Range(1, 10, MaximumIsExclusive = true, MinimumIsExclusive = true)]
     public int Rating {get; set;}
 
     [ForeignKey("MovieId")]
